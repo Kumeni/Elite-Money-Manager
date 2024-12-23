7010b9bc14260356198ca8e3dae4a62aba4b7896
Kumeni <samsonkumeni8@gmail.com>
2024-12-24 00:18:42 +0300

    Automation form

---
+++ scripts/swiper/initialize-swiper.js
@@ -0,0 +1,33 @@
+let frequency = new Swiper('.frequency', {
+	// Optional parameters
+	speed:1500,
+	spaceBetween:0,
+	slidesPerView:1,
+	direction: 'horizontal',
+	loop: true,
+	effect:'flip',
+	//effect:'cards',
+  
+	// If we need pagination
+	allowTouchMove:true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable:true,
+	},
+
+	/*autoplay:{
+		delay:4000,
+		disableOnInteraction:false,
+	},*/
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+});
